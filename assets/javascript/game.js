297bbeaab9ecf0cb473e1efacbba99dc33a9776d
Jake Owens <Jowens@Jakes-Air-3.attlocal.net>
2017-02-22 02:22:12 -0500

    fourth commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,28 @@
+	var randomNumber = Math.floor(Math.random() * (120-19+1)) + 19;
+	console.log(randomNumber)
+
+		//randomNumber.addClass("compPick");
+		//randomNumber=$("#compPick")
+	$(document).ready(function(){
+		//$("randomNumber").addClass("compPick");
+		document.getElementById('compRandomNumber').innerHTML = randomNumber;
+		document.getElementById('runningTotal').innerHTML = 24;
+		console.log(compRandomNumber)
+});
+		//document.write("<p>" + randomNumber+ "</p>");
+		/*<script src="assets/javascript/game.js">*/
+		var counter =0;
+			$(".crystal-image").on("click",function() {
+				counter+=1
+				var crystalRandomNumber = Math.floor(Math.random() * (12-1)) + 1;
+		
+			alert("Crystal Random Number is  "+crystalRandomNumber);
+	});	
+	
 
   
 
 
 
   
-
-var imageCrystal = $("<img>");
-  imageCrystal.addClass("crystal-image");
    
